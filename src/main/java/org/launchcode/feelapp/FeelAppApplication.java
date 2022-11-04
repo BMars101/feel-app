2d3620ef5c7da085dce6e4566b1559f831c17ad2
Beth Day <beth.ellieday@gmail.com>
2022-11-04 11:10:37 -0400

    adding starter files to project repo

---
+++ src/main/java/org/launchcode/feelapp/FeelAppApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.feelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/feelapp/FeelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.feelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
