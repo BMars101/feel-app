6436311dd2ad89868ba525522679c0d43d746d1e
freelyPheobe <104399179+freelyPheobe@users.noreply.github.com>
2022-11-09 16:56:42 -0500

    Started profile

---
+++ src/main/java/org/launchcode/feelapp/controllers/ProfileController.java
@@ -0,0 +1,26 @@
+package org.launchcode.feelapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("My Profile")
+    @ResponseBody
+    public String name(@PathVariable String name) {
+        return "<html><body>"+
+                "<h2> My Profile</h2>"+
+                "</body></html>";
+    }
+
+
+   /* @GetMapping("My Profile")
+    @ResponseBody
+    public String nameoftheHydrator(@PathVariable String name, @RequestParam int age) {
+        return name + "Hydrated!";
+    }*/
+}
