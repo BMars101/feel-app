bd7f6c24ea5424126abc83b10b8a3dd93ad8085c
Beth Day <beth.ellieday@gmail.com>
2022-12-09 10:17:28 -0500

    Took out unnecessary code in html file and modified homescreencontroller

--- src/main/java/org/launchcode/feelapp/controllers/HomescreenController.java
+++ src/main/java/org/launchcode/feelapp/controllers/HomescreenController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -16,8 +18,14 @@ public class HomescreenController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthenticationController authenticationController;
+
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="home-page")
-    public String displayUserHomePage(Model model){
+    public String displayUserHomePage(HttpServletRequest request, Model model){
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+        model.addAttribute("username", "Hi " + user.getUsername() + "!");
         return "home-page";
     }
 
